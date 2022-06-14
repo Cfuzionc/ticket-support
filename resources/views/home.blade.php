49b78a314cb86cd032ff5a6c7785f8c8a1aa8f44
Pasha <57643388+Cfuzionc@users.noreply.github.com>
2022-06-14 15:41:25 +0200

    Ticket migration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -14,7 +14,22 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <form>
+                        <div class="form-group">
+                          <label for="exampleFormControlInput1">Subject</label>
+                          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter your ticket subject"><br>
+                        </div>
+                        <div class="form-group">
+                          <label for="exampleFormControlTextarea1">Description</label>
+                          <textarea placeholder="Enter the issue at question" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea><br>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlFile1">Add screenshot / video <b>(optional)<b></label>
+                            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                            <button type="submit" class="btn btn-primary ticket_submit">Submit</button>
+                          </div>
+
+                      </form>
                 </div>
             </div>
         </div>
